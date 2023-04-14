885bc5e17f0192c63f8e375d71ce063ff4914376
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-14 09:20:07 -0600

    Init commit

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,32 @@
+require 'rspec'
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  before(:each) do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+  end
+  
+  describe '#initialize' do
+    it 'exists' do
+      expect(@diana).to be_a(Candidate)
+    end
+
+    it 'has attributes' do
+      expect(@diana.name).to eq ("Diana D")
+      expect(@diana.party).to eq (:democrat)
+    end
+
+    it 'has no votes' do
+      expect(@diana.votes).to eq (0)
+    end
+
+    it 'has votes' do
+      @diana.vote_for!
+      @diana.vote_for!
+      @diana.vote_for!
+      expect(@diana.votes).to eq (3)
+      @diana.vote_for!
+      expect(@diana.votes).to eq (4)
+    end
+  end
+end
